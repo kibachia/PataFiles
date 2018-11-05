0ccfa9769cb1443198b644a39d8ffbe29b0d20f1
Wainaina Kibachia <pwainainak@gmail.com>
2018-11-05 14:11:21 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?PHP
+##################  Access Login ###############
+
+$userName       = "admin@patafiles.com"; //Your username
+$passWord       = "patafiles"; //Your Password
+
+################################################
+$limitAccess    = $_SERVER["DOCUMENT_ROOT"];
+
+$_users = array();
+?>
